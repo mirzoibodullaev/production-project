1e91bc379791c46217ff9e9849c41ddae405f42d
mirzoibodullaev <1bodullaevm11@gmail.com>
2025-02-11 16:53:22 +0500

    chore(webpack): split config into modules, add new options

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,21 @@
 import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildPath } from "./config/build/types/config";
 
-const config: webpack.Configuration = {
-    mode: "development",
-    entry: "./src/index.ts",
-    output: {
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "public", "index.html"),
-        }),
-        new webpack.ProgressPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
+const paths: BuildPath = {
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
 };
+
+const mode = "development";
+const isDev = mode === "development";
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: "development",
+    paths,
+    isDev,
+});
 
 export default config;
