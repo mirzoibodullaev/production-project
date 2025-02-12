288c25891b9701716501f6bccc138614754a7226
mirzoibodullaev <1bodullaevm11@gmail.com>
2025-02-12 19:27:37 +0500

    feat: implement routeConfig and AppRouter for routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,7 @@
-import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
 import { useTheme } from "./providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "./providers/router";
 
 import "./styles/index.scss";
 
@@ -15,12 +12,7 @@ export const App = () => {
             <button onClick={toggleTheme}>toggle</button>
             <Link to={"/"}>Главная</Link>
             <Link to={"/about"}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
