4d3b57889fd8b5e90d0bf2c0c597a53135581dd4
mirzoibodullaev <1bodullaevm11@gmail.com>
2025-02-12 15:23:39 +0500

    feat: add routing, add AboutPage and MainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import "./index.scss";
 
 export const App = () => {
-    return <div className="app">App</div>;
+    return (
+        <div className="app">
+            <Link to={"/"}>Главная</Link>
+            <Link to={"/about"}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<MainPageAsync />} />
+                    <Route path="/about" element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
+        </div>
+    );
 };
