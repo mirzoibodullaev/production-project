b80dd5af8c55fbcc76714e6dd63b4342bdca6694
mirzoibodullaev <1bodullaevm11@gmail.com>
2025-02-13 16:03:10 +0500

    feat: add Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,13 +4,17 @@ import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 
 import "./styles/index.scss";
+import { Sidebar } from "widgets/Sidebar";
 
 export const App = () => {
     const { theme } = useTheme();
     return (
         <div className={classNames("app", {}, [theme])}>
             <Navbar />
-            <AppRouter />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
         </div>
     );
 };
