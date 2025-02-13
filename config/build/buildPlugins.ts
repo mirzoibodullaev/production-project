352dcb35f567f317754881ec1967067c3adfb370
mirzoibodullaev <1bodullaevm11@gmail.com>
2025-02-13 21:18:52 +0500

    chore: add BundleAnalyzer for bundle size inspection

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 import { BuildOptions } from "./types/config";
 export function buildPlugins({
@@ -18,6 +19,9 @@ export function buildPlugins({
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
         }),
     ];
 }
