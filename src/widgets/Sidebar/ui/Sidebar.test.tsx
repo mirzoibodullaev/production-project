5ca57b1c7012815984a30b1fb325ae15f63a2555
mirzoibodullaev <1bodullaevm11@gmail.com>
2025-02-21 19:03:18 +0500

    fix: fix unit Sidebar.test.tsx

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -4,14 +4,17 @@ import { I18nextProvider } from "react-i18next";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { Sidebar } from "./Sidebar";
 import { ThemeProvider } from "app/providers/ThemeProvider";
+import { StoreProvider } from "app/providers/StoreProvider";
 import i18n from "shared/config/i18n/i18nForTests";
 
 const renderWithProviders = (component: JSX.Element) => {
     return render(
         <MemoryRouter>
-            <I18nextProvider i18n={i18n}>
-                <ThemeProvider>{component}</ThemeProvider>
-            </I18nextProvider>
+            <StoreProvider>
+                <I18nextProvider i18n={i18n}>
+                    <ThemeProvider>{component}</ThemeProvider>
+                </I18nextProvider>
+            </StoreProvider>
         </MemoryRouter>
     );
 };
