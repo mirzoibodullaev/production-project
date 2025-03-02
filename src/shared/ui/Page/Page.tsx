ab9bcf9cb4827ed6a5ef9570260dc0cb9442b00a
mirzoibodullaev <1bodullaevm11@gmail.com>
2025-03-02 13:57:36 +0500

    fix: fix scroll

--- src/shared/ui/Page/Page.tsx
+++ src/shared/ui/Page/Page.tsx
@@ -54,7 +54,7 @@ export const Page = ({ className, children, onScrollEnd }: PageProps) => {
             className={classNames(cls.Page, {}, [className])}
         >
             {children}
-            <div ref={triggerRef} />
+            {onScrollEnd ? <div className={cls.trigger} ref={triggerRef} /> : null}
         </section>
     );
 };
